432831fc6b3a60beadba55066cdbc5db61d77f44
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-11-29 21:56:56 +0600

    axios-request-http-apply

---
+++ src/http-request-axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const request = axios.create({
+    baseURL: "http://localhost:5000",
+    headers: {
+        "content-type": "application/json",
+    }
+});
+
+export default request;
