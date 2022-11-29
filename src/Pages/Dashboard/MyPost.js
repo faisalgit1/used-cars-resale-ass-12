e1fe1e588fec2403e19ea1df43ef9a313f7c0fb7
faisalgit1 <mdfaisalahmed520@gmail.com>
2022-11-29 17:12:27 +0600

    add-car-post-card

--- src/Pages/Dashboard/MyPost.js
+++ src/Pages/Dashboard/MyPost.js
@@ -1,8 +1,75 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useState } from 'react';
+import { useContext } from 'react';
+import toast from 'react-hot-toast';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider';
+import MyPostCard from '../Dashboard/MyPostCard'
+
 
 const MyPost = () => {
+
+    const { user } = useContext(AuthContext)
+    const [cardetails, setcardetails] = useState(null)
+
+    const { data: cars, isLoading, refetch } = useQuery({
+        queryKey: ['cars', user?.email],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/allcars?email=${user?.email}`, {
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('Token')}`
+                }
+            })
+            const data = await res.json()
+            return data
+        }
+    })
+
+    if (isLoading) {
+        return <p>Loading..</p>
+    }
+
+
+    const handleAdvertise = (id) => {
+        fetch(`http://localhost:5000/advertiseCar/${id}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('Token')}`
+
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                refetch()
+                toast.success('Product is Live on Ad Section')
+            })
+    }
+
+
     return (
-        <div>
+        <div className='h-screen w-full'>
+            <h1 className='text-xl text-center my-10 font-semibold'>You have {cars.length} {cars.length === 0 ? 'Product' : 'Products'}</h1>
+            {
+                cars.length === 0 && <>
+                    <p className='font-semibold text-xs my-4 text-center'>Wand To <Link to='/dashboard/addaproduct' className='underline'>add a Product</Link></p>
+                </>
+            }
+            <div className='px-4 md:px-32'>
+                {/* BIKEs Card  */}
+                {
+                    cars.map(car => <MyPostCard
+                        key={car._id}
+                        car={car}
+                        handleAdvertise={handleAdvertise}
+                        setcardetails={setcardetails}
+                    >
+                    </MyPostCard>)
+                }
+
+
+            </div>
 
         </div>
     );
---
+++ src/Pages/Dashboard/MyPostCard.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MyBikesCard = ({ car, handleAdvertise, setcardetails }) => {
+
+
+    const {
+        _id,
+        model,
+        image,
+        postdate,
+        condition,
+        totalDriven,
+        orginalPrice,
+        resalePrice,
+        sellerNumber,
+        sellerLocation,
+        status,
+        paid,
+
+    } = car;
+
+
+
+
+    return (
+        <div>
+            <div className='flex flex-col md:flex-row  select-none shadow-lg bg-white rounded-lg p-2 w-full md:w-[700px] relative my-4 border items-center font-semibold gap-5'>
+                <img className='w-32' src={image} alt="" />
+                <div>
+                    <h1 className='text-xl'>
+                        {model.slice(0, 15)}
+                    </h1>
+                    <div>
+                        <p>
+                            Orginal price:{orginalPrice}
+                        </p>
+                        <p>
+                            Resale-price:{resalePrice}
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    {
+                        status === 'available' && paid === 'false' ?
+                            <>
+                                <p className='bg-green-500 text-white rounded-full px-3'>Available</p>
+                            </>
+                            :
+                            <>
+                                <p className='bg-red-500 w-14 text-white rounded-full px-3'>Sold</p>
+
+                            </>
+                    }
+                    <p>{postdate}</p>
+                    <p>{sellerNumber}</p>
+                </div>
+                <div>
+                    <p>
+                        Car Condition : {condition}
+                    </p>
+
+                    <p>
+                        Location: {sellerLocation}
+                    </p>
+                    <p>
+                        Total Driven: {totalDriven} km
+                    </p>
+                </div>
+                <div className='absolute right-2'>
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className=" m-1">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+                            </svg>
+
+
+                        </label>
+                        <ul tabIndex={0} className="dropdown-content menu p-2 shadow-lg bg-gray-100 rounded-box w-52">
+
+                            {status === 'available' && paid === 'false' && <>
+                                <li>
+                                    <button  >
+                                        Sold
+                                    </button>
+                                </li>
+                                <li>
+                                    {
+                                        car.advertise === 'true' ?
+                                            <>
+                                                <p className='py-0'>
+                                                    Product is Live on
+                                                </p>
+                                                <Link to='/' className='underline'>Advertise Seciton</Link>
+
+                                            </>
+                                            :
+                                            <>
+                                                <button onClick={() => handleAdvertise(_id)}>
+                                                    Advertise
+                                                </button>
+                                            </>
+                                    }
+
+                                </li>
+
+                            </>}
+                            <li>
+
+                                <label
+                                    onClick={() => setcardetails(car)}
+                                    htmlFor="editdetailsModal">Edit</label>
+                            </li>
+                            <li><button className='text-red-500'>Delete</button></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MyBikesCard;
